464fb894260dd6621a912b645fd985001cd9afca
GuhaAG <guha.agt@gmail.com>
2019-12-28 05:10:17 +0900

    Fix Read server address for server side update password page from application properties

--- src/main/java/com/bourbonandcoding/authserver/controllers/views/UserViewController.java
+++ src/main/java/com/bourbonandcoding/authserver/controllers/views/UserViewController.java
@@ -6,12 +6,15 @@ import com.bourbonandcoding.authserver.models.models.LoginUserBuilder;
 import com.bourbonandcoding.authserver.services.UserManagementService;
 import com.bourbonandcoding.authserver.utils.JwtTokenUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.Optional;
 
 @Controller
@@ -23,10 +26,18 @@ public class UserViewController {
     @Autowired
     private UserManagementService service;
 
+    @Autowired
+    Environment environment;
+
     @RequestMapping("/UpdatePassword")
-    public String showChangePasswordPage(@RequestParam("token") String token, final Model model) {
+    public String showChangePasswordPage(@RequestParam("token") String token, final Model model) throws UnknownHostException {
+        String host = environment.getProperty("server.host");
+        String port = environment.getProperty("server.port");
+
         model.addAttribute("username", jwtTokenUtil.getUsernameFromToken(token));
         model.addAttribute("token", token);
+        model.addAttribute("server_address", host + ":" + port);
+
         if (!jwtTokenUtil.validateToken(token)) {
             return "errorPage";
         }
